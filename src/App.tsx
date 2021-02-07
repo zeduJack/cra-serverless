40367fced44e4e415d08278da9e04a1a7a580847
Zeljko Dujmovic <zeljko.dujmovic83@gmail.com>
2021-02-07 21:50:51 +0100

    no styles in css

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Container from '@material-ui/core/Container'
 import Header from './components/common/Header'
 import SideNav from './components/common/SideNav'
 import Footer from './components/common/Footer'
+import { createGlobalStyle } from 'styled-components'
 
 import Page from './components/page/Index';
 
@@ -13,8 +14,8 @@ import { Nav } from './nav';
 import { angebote } from './angebote';
 
 import theme from './theme';
-import './normalize.css';
-import './index.scss';
+//import './normalize.css';
+//import './index.scss';
 import { Helmet } from 'react-helmet-async';
 
 const useStyles = makeStyles(() => ({
@@ -36,14 +37,40 @@ angebote.forEach(angebot => {
   });
 });
 
+
+
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: "Metropolis", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial,
+        "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    font-weight: 400;
+  }
+
+  html,
+  body,
+  #root {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+    font-size: 1.2rem;
+    font-weight: 400;
+  }
+`
+
+
 const App: React.FC = () => {
   const [sidenavOpen, setSidenavOpen] = useState(false);
   const classes = useStyles();
   return (
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <Helmet>
         <title>Fotografin für Portrait, Hochzeit, Familie, Event</title>
         <meta name="description" content="Professionelle Fotografin für Portraits, Hochzeiten, Familienfotos und Events"></meta>
+        <link rel="stylesheet" href="./normalize.css" />
       </Helmet>
 
       <CssBaseline />
